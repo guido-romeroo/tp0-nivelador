ee42b9351268f51fa458367651edec9b90d6b26d
guido <gromerok@fi.uba.ar>
2026-08-30 12:01:35 -0300

    agregada la implementación de deserealización de un Bet

--- services/server/src/server/server.py
+++ services/server/src/server/server.py
@@ -1,10 +1,43 @@
 import socket
 import logger
 import safe_socket
+from lottery.bet import Bet
 
 _ECHO_SERVER_MESSAGE_SIZE = 1024
 
+def bet_from_bytes(data: bytes) -> Bet:
+    offset = 0
+    agency_id = int.from_bytes(data[offset:offset+2], byteorder="big")
+    offset += 2
 
+    first_name_length = int.from_bytes(data[offset], byteorder="big")
+    offset += 1
+    first_name = data[offset:offset+first_name_length].decode("utf-8")
+    offset += first_name_length
+
+    last_name_length = int.from_bytes(data[offset], byteorder="big")
+    offset += 1
+    last_name = data[offset:offset+last_name_length].decode("utf-8")
+    offset += last_name_length
+
+    document = int.from_bytes(data[offset:offset+4], byteorder="big")
+    offset += 4
+
+    birthdate_length = int.from_bytes(data[offset], byteorder="big")
+    offset += 1
+    birthdate = data[offset:offset+birthdate_length].decode("utf-8")
+    offset += birthdate_length
+
+    number = int.from_bytes(data[offset:offset+2], byteorder="big")
+
+    return Bet(
+        agency_id=agency_id,
+        first_name=first_name,
+        last_name=last_name,
+        document=document,
+        birthdate=birthdate,
+        number=number
+    )
 class Server:
     def __init__(self, server_host: str, server_port: int) -> None:
         self.server_host = server_host
