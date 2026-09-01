2aa7fd8f1e19c65b43a92143ed30790630d8632b
guido <gromerok@fi.uba.ar>
2026-09-01 10:33:32 -0300

    se implementa el manejo de la conexion (protocolo) en el client

---
+++ services/client/src/client/connection_facilitator.go
@@ -0,0 +1,116 @@
+package client
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/logger"
+	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/safe_socket"
+)
+
+const CONNECTION_ATTEMPTS_MAX = 3
+const CONNECTION_ATTEMPS_DELAY_MS = 200
+
+type ConnectionConfig struct {
+	Host string
+	Port string
+}
+
+type ConnectionFacilitator struct {
+	host           string
+	port           string
+	headerSize     int
+	maxMessageSize int
+	connection     net.Conn
+}
+
+// El protocolo actual es simplemente enviar un mensaje con un header de 2 bytes que indica el tamaño del mensaje, seguido del mensaje en sí.
+// Al hacer un recv, se lee primero el header de 2 bytes para saber el tamaño del mensaje, y luego se lee el mensaje completo.
+func NewConnectionFacilitator(config *ConnectionConfig) (*ConnectionFacilitator, error) {
+	connection, err := connectToServer(config.Host, config.Port)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ConnectionFacilitator{
+		host:           config.Host,
+		port:           config.Port,
+		headerSize:     2,
+		maxMessageSize: 65535,
+		connection:     connection,
+	}, nil
+}
+
+func connectToServer(host, port string) (net.Conn, error) {
+	const action = "connect-to-server"
+	var err error
+	var conn net.Conn
+
+	logger.Info(action, logger.InProgress)
+	for i := range CONNECTION_ATTEMPTS_MAX {
+		conn, err = net.Dial("tcp", host+":"+port)
+		if err != nil {
+			logger.Warn(action, logger.Fail, "attempt", i)
+			time.Sleep(CONNECTION_ATTEMPS_DELAY_MS * time.Millisecond)
+			continue
+		}
+
+		logger.Info(action, logger.Success)
+		break
+	}
+
+	return conn, err
+}
+
+func (connectionFacilitator *ConnectionFacilitator) Close() error {
+	if connectionFacilitator.connection != nil {
+		return connectionFacilitator.connection.Close()
+	}
+	return nil
+}
+
+func (connectionFacilitator *ConnectionFacilitator) checkMessageSize(message []byte) error {
+
+	messageSize := int(len(message))
+	if messageSize > connectionFacilitator.maxMessageSize {
+		return fmt.Errorf("message size exceeds maximum allowed size of %d bytes", connectionFacilitator.maxMessageSize)
+	}
+	return nil
+}
+
+func (connectionFacilitator *ConnectionFacilitator) addHeaderToMessage(message []byte) ([]byte, error) {
+	if err := connectionFacilitator.checkMessageSize(message); err != nil {
+		return nil, err
+	}
+	messagewithHeader := make([]byte, connectionFacilitator.headerSize+len(message))
+
+	binary.BigEndian.PutUint16(messagewithHeader[0:2], uint16(len(message)))
+	if copy(messagewithHeader[connectionFacilitator.headerSize:], message) != len(message) {
+		return nil, fmt.Errorf("unexpected error while copying message to buffer with header")
+	}
+	return messagewithHeader, nil
+}
+
+func (connectionFacilitator *ConnectionFacilitator) Send(message []byte) error {
+	messagewithHeader, err := connectionFacilitator.addHeaderToMessage(message)
+	if err != nil {
+		return err
+	}
+	return safe_socket.SendAll(connectionFacilitator.connection, messagewithHeader)
+}
+
+func (connectionFacilitator *ConnectionFacilitator) Recv() ([]byte, error) {
+
+	messageLen, err := safe_socket.RecvAll(connectionFacilitator.connection, connectionFacilitator.headerSize)
+	if err != nil {
+		return nil, err
+	}
+
+	message, err := safe_socket.RecvAll(connectionFacilitator.connection, int(binary.BigEndian.Uint16(messageLen)))
+	if err != nil {
+		return nil, err
+	}
+	return message, nil
+}
