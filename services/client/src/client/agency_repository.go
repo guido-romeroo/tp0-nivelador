5ef8a01d91ad7f91f833712c29588b81126726b9
guido <gromerok@fi.uba.ar>
2026-09-10 13:07:55 -0300

    feat: se pasa a usar strings en lugar de encoding/csv

--- services/client/src/client/agency_repository.go
+++ services/client/src/client/agency_repository.go
@@ -1,10 +1,12 @@
 package client
 
 import (
-	"encoding/csv"
+	"bufio"
 	"errors"
+	"io"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type AgencyConfig struct {
@@ -13,10 +15,9 @@ type AgencyConfig struct {
 }
 
 type AgencyRepository struct {
-	bets        *csv.Reader
-	winners     *csv.Writer
 	betsFile    *os.File
 	winnersFile *os.File
+	bets        *bufio.Scanner
 }
 
 func NewAgencyRepository(config *AgencyConfig) (*AgencyRepository, error) {
@@ -27,24 +28,28 @@ func NewAgencyRepository(config *AgencyConfig) (*AgencyRepository, error) {
 
 	winnersFile, err := os.OpenFile(config.WinnersFilePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		betsFile.Close()
-		return nil, err
+		errClose := betsFile.Close()
+		return nil, errors.Join(err, errClose)
 	}
 
 	return &AgencyRepository{
 		betsFile:    betsFile,
 		winnersFile: winnersFile,
-		bets:        csv.NewReader(betsFile),
-		winners:     csv.NewWriter(winnersFile),
+		bets:        bufio.NewScanner(betsFile),
 	}, nil
 }
 
 func (repo *AgencyRepository) NextBet(agencyId uint16) (*Bet, error) {
-	record, err := repo.bets.Read()
-	if err != nil {
-		return nil, err
+	if !repo.bets.Scan() {
+		if err := repo.bets.Err(); err != nil {
+			return nil, err
+		}
+		return nil, io.EOF
 	}
-
+	record := strings.Split(repo.bets.Text(), ",")
+	if len(record) != 5 {
+		return nil, errors.New("invalid record format: expected 5 fields")
+	}
 	firstName := record[0]
 	lastName := record[1]
 
@@ -53,6 +58,7 @@ func (repo *AgencyRepository) NextBet(agencyId uint16) (*Bet, error) {
 		return nil, err
 	}
 	document := uint32(val)
+
 	birthdate := record[3]
 
 	val, err = strconv.ParseUint(record[4], 10, 32)
@@ -61,9 +67,14 @@ func (repo *AgencyRepository) NextBet(agencyId uint16) (*Bet, error) {
 	}
 	number := uint32(val)
 
-	bet, err := NewBet(agencyId, firstName, lastName, document, birthdate, number)
-
-	return bet, nil
+	return NewBet(
+		agencyId,
+		firstName,
+		lastName,
+		document,
+		birthdate,
+		number,
+	)
 }
 
 func (repo *AgencyRepository) WriteWinner(bet *Bet) error {
@@ -75,23 +86,14 @@ func (repo *AgencyRepository) WriteWinner(bet *Bet) error {
 		strconv.FormatUint(uint64(bet.number), 10),
 	}
 
-	if err := repo.winners.Write(record); err != nil {
-		return err
-	}
-
-	repo.winners.Flush()
-
-	return repo.winners.Error()
+	line := strings.Join(record, ",") + "\n"
+	_, err := repo.winnersFile.WriteString(line)
+	return err
 }
 
 func (repo *AgencyRepository) Close() error {
-	repo.winners.Flush()
-
-	errFlush := repo.winners.Error()
-
 	errWinnersClose := repo.winnersFile.Close()
-
 	errBetsClose := repo.betsFile.Close()
 
-	return errors.Join(errFlush, errWinnersClose, errBetsClose)
+	return errors.Join(errWinnersClose, errBetsClose)
 }
